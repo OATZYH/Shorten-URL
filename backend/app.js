d0e307c4d8d56e3f9173a03fffc7bd7dda9fd65f
Sarun Khumthai <srnkhumthai@gmail.com>
2024-11-06 10:49:47 +0700

    Fix cors

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://synerry-test.vercel.app'],  
+  origin: ['http://localhost:5173', 'https://synerry-test.vercel.app'],  
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
