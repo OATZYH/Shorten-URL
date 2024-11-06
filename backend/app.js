784c7ab1d26d29163fb353e23ff547d9d7e74805
Sarun Khumthai <srnkhumthai@gmail.com>
2024-11-06 11:24:24 +0700

    update cors

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // CORS Configuration
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://synerry-test.vercel.app'],
+  origin: ['http://localhost:5173', 'https://synerry-test.vercel.app','https://synerry-test.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
