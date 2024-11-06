fb004df9fbede7d5f144fe920a06473b62deb260
Sarun Khumthai <srnkhumthai@gmail.com>
2024-11-06 15:00:52 +0700

    fix: :zap: Modify encoding
    handle both long text and long number

--- backend/utils/encoding.js
+++ backend/utils/encoding.js
@@ -1,15 +1,29 @@
 const BASE62 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const crypto = require('crypto');
 
-function generateRandomString(length = 10) {
-  let randomString = '';
-  
-  for (let i = 0; i < length; i++) {
-    // Generate a random index based on the length of BASE62
-    const randomIndex = crypto.randomInt(0, BASE62.length);
-    randomString += BASE62[randomIndex];
+function base62Encode(buffer) {
+  let result = '';
+  let num = BigInt('0x' + buffer.toString('hex'));
+  const base = BigInt(BASE62.length);
+  while (num > 0) {
+    const mod = num % base;
+    result = BASE62[mod] + result;
+    num = num / base;
   }
-  return randomString;
+  return result;
 }
 
-module.exports = { generateRandomString };
+function generateShortCode(url, length = 8) {
+  // Create a hash of the URL
+  const hash = crypto.createHash('sha256').update(url).digest();
+
+  // Encode the hash into a base62 string
+  const fullCode = base62Encode(hash);
+
+  // Use the first 'length' characters as the short code
+  const shortCode = fullCode.substring(0, length);
+
+  return shortCode;
+}
+
+module.exports = { generateShortCode  };
